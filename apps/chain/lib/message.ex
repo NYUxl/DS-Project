82cccd105184495ad23a23abb13a59885280980c
nyu-distributed-systems <xl3399@nyu.edu>
2022-11-20 09:56:07 +0000

    State response

--- apps/chain/lib/message.ex
+++ apps/chain/lib/message.ex
@@ -57,15 +57,24 @@ defmodule Server.NewInstance do
     end
 end
 
-defmodule Server.Hold do
+defmodule Server.StateResponse do
     @moduledoc """
     When some node fails, orchestrator need to temporaliy pause
     all the nodes
     """
     alias __MODULE__
     defstruct(
-        default: nil
+        id: nil,
+        state: nil
     )
+
+    @spec new(any()) :: %StateResponse{}
+    def new(id, nf_state) do
+        %StateResponse{
+            id: id,
+            state: nf_state
+        }
+    end
 end
 
 defmodule SomeName do
